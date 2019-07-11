bf8a61d2428d4eb0e29aa6ebb83d818205f6d8f0
vibhu mahendru <vibhumahendru@gmail.com>
2019-07-11 12:22:36 +0530

    render json

--- src/products/models.py
+++ src/products/models.py
@@ -7,4 +7,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(blank=True, null=False) #default='this is coool vibhu'
-    featured = models.BooleanField()
+    featured = models.BooleanField(default=False)
--- src/products/views.py
+++ src/products/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
+from django.core import serializers
+from django.http import HttpResponse
+
 # Create your views here.
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+        print("YO YO YO")
+    context = {
+        "form":form
+    }
+
+    return render(request, 'products/product_create.html',context)
+
+
+
+
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
-
+    products = Product.objects.all()
+    data = serializers.serialize('json', products)
+    print(products)
     context = {
         "object":obj
     }
 
 
+    # return HttpResponse(data, content_type='application/json')
+    return render(request, 'products/product_detail.html',context)
 
-    return render(request, 'product/detail.html',context)
+
+
+
+
+
+
+
+
+
+
+    ##
